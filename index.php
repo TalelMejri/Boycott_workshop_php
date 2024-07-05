37332251adcf9a8c66c159db9054a00770b1c5f8
TalelMejri <talelmejri8@gmail.com>
2024-07-05 22:39:31 +0100

    Affichage users

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-    
-    require_once('./db_connect/index.php');
 
+    require_once('./db_connect/index.php');
     $users=$pdo->prepare("select * from users");
     $users->execute();
-
     $res=$users->fetchAll();
+    
+    $page_titel="List Users";
+    $template="index";
 
-    include "./layout.php";
+    include "./layout.phtml";
 ?>
+
